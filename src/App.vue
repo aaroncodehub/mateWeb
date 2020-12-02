97ef12ce327d7e0e32ec0658251b4770b39ceb20
Aaron <71854919+aaroncodehub@users.noreply.github.com>
2020-12-02 14:07:43 +1300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app class="grey lighten-4">
-    <v-content>
+    <v-main>
       <Navigation v-if="currentUser"></Navigation>
     <router-view/>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
